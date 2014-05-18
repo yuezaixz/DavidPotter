17182b041ce4eccf0eeca83ef8f5dc1e485f5c07
David <xiao303178394@gmail.com>
2014-05-18 18:48:11 +0800

    修改Import
    去除不必要的import

--- src/main/java/com/david/codejourney/salestrategy/responsechain/handler/IHandler.java
+++ src/main/java/com/david/codejourney/salestrategy/responsechain/handler/IHandler.java
@@ -1,6 +1,5 @@
 package com.david.codejourney.salestrategy.responsechain.handler;
 
-import com.david.codejourney.salestrategy.basket.Basket;
 import com.david.codejourney.salestrategy.calc.ICalculate;
 import com.david.codejourney.salestrategy.strategy.PotterStrategy;
 
